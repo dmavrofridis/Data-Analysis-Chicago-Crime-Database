54a2f1082b4ffd788296f1baebaab4f4142eb7ad
MagoriaPhantasmo <donaldbaracskay2022@u.northwestern.edu>
2021-10-01 15:12:24 -0500

    Added questions 2 and from google doc
    took Alex's code from Google Doc and converted to sql files

---
+++ first_checkpoint/second_question.sql
@@ -0,0 +1,12 @@
+/*
+ second_question.sql: Displays per year (incident)
+    1) number of officer complaints
+
+
+ */
+select EXTRACT(YEAR FROM incident_date) as incident, is_officer_complaint,
+       count(*) as count from data_allegation
+    where EXTRACT(YEAR FROM incident_date) > 2000
+        and EXTRACT(YEAR FROM incident_date) < 2020
+group by  incident, is_officer_complaint
+order by  incident, is_officer_complaint
