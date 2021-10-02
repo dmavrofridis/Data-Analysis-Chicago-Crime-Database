d659397d4437a3d1aff938784a9f1fe4a4bf87d3
dmavrofridis <91025661+dmavrofridis@users.noreply.github.com>
2021-10-02 16:03:37 -0500

    Added Project Proposal

--- first_checkpoint/first_question.sql
+++ first_checkpoint/first_question.sql
@@ -1,3 +1,5 @@
-select EXTRACT(YEAR FROM incident_date) as year, count(is_extracted_summary) as count from data_allegation
+select EXTRACT(YEAR FROM incident_date) as year, count(is_extracted_summary) as count
+from data_allegation
 group by year
 order by year
+
