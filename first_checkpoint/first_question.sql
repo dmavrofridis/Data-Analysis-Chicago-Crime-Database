a9625442612067bd6cd35199ecca5ec07d9b54f9
aleksandr1996NU <aleksandr.simonyan@northwestern.edu>
2021-10-15 12:37:28 -0500

    1) How many officers recieved a certian number of allegations

--- first_checkpoint/first_question.sql
+++ first_checkpoint/first_question.sql
@@ -4,8 +4,22 @@
 --order by year
 
 
--- the number of allegations group by officer id , we clearly see that certain police officers recieved a very high number of allegations
-select count(allegation_id) as number_allegations, officer_id from data_officerallegation
+ --the number of allegations group by officer id , we clearly see that certain police officers recieved a very high number of allegations
+with groups_of_cops  as( select count(allegation_id) as number_allegations, officer_id from data_officerallegation
 group by officer_id
 order by number_allegations desc
+    )
+
+
+
+
+select count(officer_id), number_allegations from  groups_of_cops
+group by number_allegations
+order by  number_allegations
+
+
+--select count(allegation_id) as number_allegations,  data_officerallegation.officer_id from data_officerallegation
+--join data_award on (select count(*))
+
+
 
