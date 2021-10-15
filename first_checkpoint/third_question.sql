153924a06ed688e8796f26e46e78adcfe49e173d
dmavrofridis <91025661+dmavrofridis@users.noreply.github.com>
2021-10-15 16:49:11 -0500

    Delete first_checkpoint directory

--- first_checkpoint/third_question.sql
+++
@@ -1,8 +0,0 @@
-/*
-    third_question.sql : displays by quarter (or season)
-1) number of allegations
- */
-select  EXTRACT(QUARTER FROM incident_date) as quarter,
-       count(*) as count from data_allegation
-group by quarter
-order by quarter
