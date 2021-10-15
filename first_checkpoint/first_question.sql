70175985761714f9db49ffab411a41130ea12f2b
aleksandr1996NU <aleksandr.simonyan@northwestern.edu>
2021-10-15 12:16:19 -0500

    1) a)

--- first_checkpoint/first_question.sql
+++ first_checkpoint/first_question.sql
@@ -1,5 +1,11 @@
-select EXTRACT(YEAR FROM incident_date) as year, count(is_extracted_summary) as count
-from data_allegation
-group by year
-order by year
+--select EXTRACT(YEAR FROM incident_date) as year, count(is_extracted_summary) as count
+--from data_allegation
+--group by year
+--order by year
+
+
+-- the number of allegations group by officer id , we clearly see that certain police officers recieved a very high number of allegations
+select count(allegation_id) as number_allegations, officer_id from data_officerallegation
+group by officer_id
+order by number_allegations desc
 
