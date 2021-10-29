87f59978e4a8b6d9fbf75d48d3ab3b15cb527da4
Aleksandr Simonyan <91096331+aleksandr1996NU@users.noreply.github.com>
2021-10-28 23:42:58 -0500

    Update Question 2.sql

--- checkpoint-1/src/Question 2.sql
+++ checkpoint-1/src/Question 2.sql
@@ -1,25 +1,31 @@
---Question 2: Do officers react more violently areas of lower socioeconomic status?
-
---Construct a table which contains community areas with median income
--- and polygon information
-
-WITH area_info AS (
-    SELECT median_income,  name as community, polygon FROM data_area
+WITH area_inf AS (
+    SELECT median_income, id,  name as community, polygon FROM data_area
         WHERE data_area.area_type LIKE 'community'
-)
-,
+),
+     total_pop AS (
+    SELECT SUM(count) as population_total, area_id
+    FROM data_racepopulation
+    GROUP BY area_id
+),
+     area_info AS (
+    SELECT id, median_income,  community, polygon, population_total
+    FROM area_inf
+             JOIN  total_pop ON total_pop.area_id = area_inf.id
+),
 
 -- create table which contains action response category for each officer in a TRR
 action_response_breakdown AS (
-    SELECT trr_trr.id, trr_trr.point, cast(action_sub_category as float) FROM trr_actionresponse
+    SELECT trr_trr.id,  trr_trr.point, cast(action_sub_category as float) FROM trr_actionresponse
         JOIN trr_trr ON trr_actionresponse.trr_id = trr_trr.id
     WHERE action_sub_category IS NOT NULL
 
 )
 -- join count of high action_sub_categories on falling within community area
-SELECT count(CASE WHEN action_sub_category >= 4.0 THEN 1 END),
+, joined as (SELECT population_total, count(CASE WHEN action_sub_category >= 4.0 THEN 1 END),
        median_income, community,
        count(CASE WHEN action_sub_category >= 4.0 THEN 1 END) * 100.0 / count(CASE WHEN TRUE THEN 1 END) AS percentage_trrs_violent FROM area_info
     JOIN action_response_breakdown ON st_within(action_response_breakdown.point, area_info.polygon)
-    GROUP BY median_income, community
+    GROUP BY median_income, community,  population_total)
 
+
+select * from joined
