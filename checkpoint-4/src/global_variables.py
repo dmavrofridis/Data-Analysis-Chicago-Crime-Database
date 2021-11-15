0d1ff49f11ab6558b6e26082146438acfb2c5cb2
MagoriaPhantasmo <donaldbaracskay2022@u.northwestern.edu>
2021-11-15 11:50:43 -0600

    Added basic imports and the connection to CP4

---
+++ checkpoint-4/src/global_variables.py
@@ -0,0 +1,6 @@
+
+sql_host = "codd01.research.northwestern.edu"
+sql_port = "5432"
+sql_database = "postgres"
+sql_user = "cpdbstudent"
+sql_password = "DataSci4AI"
--- checkpoint-4/src/question_1.py
+++ checkpoint-4/src/question_1.py
@@ -1,1 +1,17 @@
 # SPARK or Graph X
+
+from pyspark import *
+from pyspark.sql import *
+from graphframes import *
+import findspark
+import pandas
+import psycopg2
+from global_variables import *
+
+
+def main():
+    conn = psycopg2.connect(host=sql_host, port=sql_host, database=sql_database, user=sql_user, password=sql_password)
+
+
+if __name__ == "__main__":
+    main()
