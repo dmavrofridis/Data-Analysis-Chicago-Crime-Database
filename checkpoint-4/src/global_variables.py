af8a9f7e88cddf5400392a88840daae5fb5d810c
MagoriaPhantasmo <donaldbaracskay2022@u.northwestern.edu>
2021-11-15 12:19:19 -0600

    Altered CP4 slightly, Need queries for Graph edges and vertices

--- checkpoint-4/src/global_variables.py
+++ checkpoint-4/src/global_variables.py
@@ -1,6 +1,10 @@
 
+
+#Database costants
 sql_host = "codd01.research.northwestern.edu"
 sql_port = "5432"
 sql_database = "postgres"
 sql_user = "cpdbstudent"
 sql_password = "DataSci4AI"
+
+#Queries
--- checkpoint-4/src/question_1.py
+++ checkpoint-4/src/question_1.py
@@ -11,7 +11,7 @@ from global_variables import *
 
 def main():
     conn = psycopg2.connect(host=sql_host, port=sql_host, database=sql_database, user=sql_user, password=sql_password)
-
+    cursor = conn.cursor()
 
 if __name__ == "__main__":
     main()
