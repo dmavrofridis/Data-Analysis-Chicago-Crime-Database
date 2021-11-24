e755c9095e15b68f1e94395a8de65bb65fd48f53
MagoriaPhantasmo <91048350+MagoriaPhantasmo@users.noreply.github.com>
2021-11-24 07:48:12 -0800

    Altered query to include various info (see description)
    Added member action, subject action, injury (both alleged and verified), and response sub category

--- checkpoint-5/src/cp5_query.sql
+++ checkpoint-5/src/cp5_query.sql
@@ -1,16 +1,21 @@
 WITH trr_descr AS (
-    SELECT trr_id, resistance_type, other_description, person,
-           subject_race, subject_gender, crid FROM trr_actionresponse
+    SELECT trr_id, resistance_type,other_description, action_sub_category,person,
+           subject_race, subject_gender, crid,
+           member_action,
+           (CASE WHEN person = 'Subject Action' THEN action else NULL END) AS subject_action,
+           subject_injured, subject_alleged_injury
+    FROM trr_actionresponse
         JOIN trr_trr tt on trr_actionresponse.trr_id = tt.id
-        WHERE person = 'Subject Action'
+--         WHERE person = 'Subject Action'
 
 )
 ,
 trr_descr_cr AS (
-    SELECT resistance_type, other_description, subject_race, subject_gender, trr_descr.crid,
-           summary, is_officer_complaint, point FROM trr_descr
+    SELECT resistance_type, action_sub_category, member_action, subject_action,
+           other_description, subject_race, subject_gender, subject_injured, subject_alleged_injury,
+           trr_descr.crid, summary, is_officer_complaint, point FROM trr_descr
         JOIN data_allegation ON data_allegation.crid = trr_descr.crid
-        WHERE summary <> ''
+        WHERE summary <> '' AND (member_action IS NOT NULL OR subject_action IS NOT NULL)
 )
 
 ,
@@ -21,7 +26,9 @@ community_polygons AS (
 
 ,
 trr_descr_cr_w_loc AS (
-    SELECT resistance_type, other_description, subject_race, subject_gender, crid,
+    SELECT resistance_type, action_sub_category, member_action, subject_action,
+           other_description, subject_race, subject_gender,
+           subject_injured, subject_alleged_injury, crid,
            summary, is_officer_complaint, community FROM trr_descr_cr
         JOIN community_polygons ON ST_WITHIN(trr_descr_cr.point, community_polygons.polygon)
 )
