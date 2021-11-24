2059035e7e79b7056070471dce37eaba7927c3d9
dmavrofridis <91025661+dmavrofridis@users.noreply.github.com>
2021-11-23 20:25:43 -0600

    Main progress, and connection to DB

---
+++ checkpoint-5/global_variables.py
@@ -0,0 +1,1 @@
+conn_string = "host='codd04.research.northwestern.edu' port='5433' dbname='postgres' user='cpdbstudent' password='DataSci4AI'"
--- checkpoint-5/src/main.py
+++ checkpoint-5/src/main.py
@@ -1,1 +1,24 @@
 # Main File for NLP
+import pandas as pd
+import psycopg2
+from global_variables import *
+
+
+def connect_to_db():
+    return psycopg2.connect(conn_string)
+
+
+def query_to_string():
+    # Open and read the file as a single buffer
+    fd = open('cp5_query.sql', 'r')
+    sqlFile = fd.read()
+    fd.close()
+    return sqlFile
+
+
+if __name__ == '__main__':
+    connection = connect_to_db()
+    sql_query = query_to_string()
+    # First step is to import the required tables
+    df = pd.read_sql(sql_query, connection)
+    df.name = "cp5"
